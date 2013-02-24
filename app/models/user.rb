03eb65c0c81376cd6e3bd4d8f7f1b15a4dffa587
YeHao <yehao127@gmail.com>
2013-02-24 11:59:11 +0800

    add email downcase test and refine User email downcase method

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,8 @@ class User < ActiveRecord::Base
   # To make sure that the email address is unique,
   # must change it to lowercase before storing to database
   # Because not all database adapters use case-sensitive indices
-  before_save { |user| user.email = email.downcase }
+  # before_save { |user| user.email = email.downcase }
+  before_save { self.email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
